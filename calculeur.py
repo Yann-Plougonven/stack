7d5af8e73ddd89df5eadd51c2f3cda66eb11c811
Yann-Plougonven <77398553+Yann-Plougonven@users.noreply.github.com>
2022-10-10 20:18:40 +0200

    Ca fonctionne mais c'est moche

--- calculeur.py
+++ calculeur.py
@@ -1,12 +1,43 @@
-expressions = ('3','4','*','2','+')
+from stack import Stack  # Ne pas mettre le .py à la fin
+
+expressions = [('3', '4', '*', '2', '+')]
 operateurs = ('+', '-', '*')
 resultats = []
 
-def resultat(expressions):
+
+def resultat(expression):
+    """Cette fonction ne retourne le bon résultat que si l'expression donnée en entrée est bonne"""
     p = Stack()  # p est instance de Stack
-    for expression in expressions:
-        if operateurs
-    
+    liste = []
+    for char in expression:  # Pour chaque caractère
+        if char in operateurs:
+            cond = True
+            while cond == True:
+                try:
+                    liste.append(p.pop())
+                    print(f"liste = {liste}")
+                except IndexError:
+                    if char == "+":
+                        result = int(liste[0]) + int(liste[1])
+                        p.push(result)
+                        cond = False
+                        liste = []
+                    if char == "-":
+                        result = int(liste[0]) - int(liste[1])
+                        p.push(result)
+                        cond = False
+                        liste = []
+                    if char == "*":
+                        result = int(liste[0]) * int(liste[1])
+                        p.push(result)
+                        cond = False
+                        liste = []
+
+        else:  # Si chiffre
+            p.push(char)
+    return p.summit()
+
 
 for e in expressions:
     resultats.append(resultat(e))
+print(resultats)
