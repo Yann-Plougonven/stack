fc2b9026947a7583d6c25b2fb9a088ec4578adc0
Yann Plougonven <plougonven-y@saint-louis29.net>
2022-10-10 14:22:23 +0200

    ajout

---
+++ calculeur.py
@@ -0,0 +1,12 @@
+expressions = ('3','4','*','2','+')
+operateurs = ('+', '-', '*')
+resultats = []
+
+def resultat(expressions):
+    p = Stack()  # p est instance de Stack
+    for expression in expressions:
+        if operateurs
+    
+
+for e in expressions:
+    resultats.append(resultat(e))
