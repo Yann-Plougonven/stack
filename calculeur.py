6ceb26ec742f5c52c0177ac6144c3e76e0443a2f
Yann-Plougonven <77398553+Yann-Plougonven@users.noreply.github.com>
2022-10-10 22:28:36 +0200

    Quasi fonctionnel (quelques pbs avec les "-")

--- calculeur.py
+++ calculeur.py
@@ -1,6 +1,7 @@
 from stack import Stack  # Ne pas mettre le .py à la fin
 
-expressions = [('3', '4', '*', '2', '+'), ('2', '4', '+'), ('1', '19', '+', '2', '-')]
+# ATTENTION : Y'a des petits problèmes pour les oppérations avec des '-' (par exemple si on remplacer '0' par '10' ci-dessous)
+expressions = [('3', '4', '*', '2', '+'), ('2', '4', '+', '6', '-', '1', '+'), ('1', '19', '+', '2', '-'), ('0', '2', '-', '2', '+', '10', '-')]
 operateurs = ('+', '-', '*')
 resultats = []
 
@@ -9,6 +10,7 @@ def resultat(expression):
     """Cette fonction ne retourne le bon résultat que si l'expression donnée en entrée est bonne"""
     p = Stack()  # p est instance de Stack
     nbs_pour_op = []
+    result = 0  # On initialise le résultat de l'opération en cours de traitement à 0.
     for char in expression:  # Pour chaque caractère de l'expression
         if char in operateurs:  # Si '+', '-', '*'
             cond = True
@@ -25,14 +27,9 @@ def resultat(expression):
                             result = result + int(nbs_pour_op[i])
 
                     elif char == "-":
-                        #result = int(nbs_pour_op[0])  # On initialise le résultat de l'opération en cours de traitement à 0.
-                        print(f"result = {result}")
                         for a in nbs_pour_op[-2]:  # Nombre de terme dans nb_pour_op -1
                             i += 1
-                            print(f"nbs_pour_op[i] = {nbs_pour_op[i]}")
                             result = result - int(nbs_pour_op[i])
-                            print(result)
-
 
                     elif char == "*":
                         result = 1 # On initialise le résultat de l'opération en cours de traitement à 1.
