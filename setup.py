615872fbe8c8708b48647636f6ecc86f21f8dc80
Yann Plougonven <plougonven-y@saint-louis29.net>
2022-10-18 14:29:32 +0200

    errors corrections

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='Stack',
-    version='0.1',
-    url='https://github.com/Yann-Plougonven/hanoi_kernel',
+    version='1.1',
+    url='https://github.com/Yann-Plougonven/stack',
     author='Yann Plougonven--Lastennet',
     author_email='plougonven-y@saint-louis29.net',
-    description='The kernel of the hanoi tower game project, made during NSI class',
+    description="A repository which contains a stack's implementation, made during NSI class",
     packages=find_packages(),    
     install_requires=[],
 )
