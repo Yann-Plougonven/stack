ed92936a26c21fa9dd6804bcfb3f6d35d97dddf1
Yann Plougonven--Lastennet <77398553+Yann-Plougonven@users.noreply.github.com>
2022-11-05 23:18:18 +0100

    Refonte du code
    Version fonctionnelle du 10 octobre 2022.

--- parser.py
+++ parser.py
@@ -2,40 +2,28 @@ from stack import Stack  # Ne pas mettre le .py à la fin
 expressions = ['2x(x+1)','2x((x+1)','2x+1)', '2x(x+1)(x+2)(x+3)','2x(x+1)(x+2)((x+3)','2x(x+1)(x+2))(x+3)','','(','()',')(']
 resultats_expressions = []
 
+codes = {")":"(", "]":"[", "}":"{"}
+
 def parse_checker(string):
     p = Stack()
     for letter in string:
-        if letter == "(":  # Ajouter les "(" à resultats_expressions
-            p.push("(")
-        if letter == "[":  # Ajouter les "[" à resultats_expressions
-            p.push("[")
-        
-        
-        elif letter == ")":  # Si ")"
-            try:  
-                if p.summit() == "(":  # Si le dernier caractère de la file est "("
-                    p.pop()   
-                else:
-                    return False 
-            except IndexError:
-                return False
-
-            
-        elif letter == "]":  # Si "]"
+        if letter in codes.values():  # Si (, [, {
+            p.push(letter)
+        elif letter in codes.keys():  # Si ), ], }
             try:
-                if p.summit() == "[":        
+                if codes[letter] == p.summit():     
                     p.pop()
                 else:
                     return False
             except IndexError:
                 return False
-            
-        
+
+
     if p.isEmpty() == True:
         return True
     else:
         return False
-    
+
 for e in expressions:
     resultats_expressions.append(parse_checker(e))
 
